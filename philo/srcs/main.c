8bdcb24d7729a96cdd96a5afb079d56f394634c8
Lea Devoudelle-Royer <ldevoude@z1r7p3.42lyon.fr>
2025-07-23 16:16:09 +0200

    misc(main.c): I tried to learn how to use the times related tool

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -3,16 +3,21 @@
 #include <time.h> //time_t type need it
 //parsing need between 1 and n philo also check if optional arg
 
+#include <unistd.h> //USLEEP
 
 
 int main(int argc, char *argv[])
 {
-    //time_t seconds;
-    // struct timeval tv;
+    time_t test;
+    struct timeval tv;
 
-    // gettimeofday(&tv, NULL);
-    // printf("Seconds since Epoch: %ld\n", tv.tv_sec);
-    // printf("Microseconds part: %ld\n", tv.tv_usec);
+    gettimeofday(&tv, NULL);
+    test = tv.tv_sec;
+    printf("Seconds since Epoch: %ld\n", tv.tv_sec - tv.tv_sec);
+    printf("Microseconds part: %ld\n", tv.tv_usec);
+    usleep(6000000);
+    gettimeofday(&tv, NULL);
+    printf("time : %ld\n", tv.tv_sec - test);
     if (parsing(argc, argv))
         printf("PARSING ERROR\n");
     else
