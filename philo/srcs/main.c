19c2a3640e8127a91bc61394256c224f4607e55f
Lea Devoudelle-Royer <ldevoude@z1r6p5.42lyon.fr>
2025-07-31 12:04:35 +0200

    feat(main.c): started the feature that would take care of counting the time that pass in miliseconds

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h> //malloc
 #include "parsing.h"
 #include <pthread.h> //phtread_t
+#include <unistd.h> //usleep
 
 //pot soucis si que un philo 
 
@@ -28,17 +29,36 @@
 
 //     while(i < set->nbr_philo)
 //     {
-//         if(pthread_create(th + i,  NULL, &routine, NULL) != 0)
+//         if(pthread_create(th + i,  NULL, &routine, philo) != 0)
 // 		    return(1);
 // 	    printf("thread %d has started\n", i);
 //     }
 // }
 
-// static void philo(t_settings *set, t_philo *philo, t_forks *forks)
-// {
-//     // thread_maker(set, philo, forks);
-//     //while(!set->death)
-// }
+static void philosopher(t_settings *set, t_philo *philo, t_forks *forks)
+{
+    time_t to_sub_unit;
+    time_t to_sub_micro;
+    struct timeval tv;
+    long result;
+
+    gettimeofday(&tv, NULL);
+    to_sub_unit = tv.tv_sec;
+    to_sub_micro = tv.tv_usec;
+    printf("Microseconds part: %ld\n", tv.tv_usec);
+    usleep(5005000);
+    gettimeofday(&tv, NULL);
+    printf("time : %ld + %ld\n", tv.tv_sec - to_sub_unit, tv.tv_usec - to_sub_micro);
+    result = (tv.tv_sec - to_sub_unit) * 1000000 + (tv.tv_usec - to_sub_micro);
+    printf("result = : %ld \n",result / 1000);
+    //printf("milisecond : %ld \n", (tv.tv_sec - to_sub_unit) * 1000  + (tv.tv_usec - to_sub_micro) / 1000);
+    set = NULL;
+    philo = NULL;
+    forks = NULL;
+    printf("nice");
+    // thread_maker(set, philo, forks);
+    //while(!set->death)
+}
 
 int main(int argc, char *argv[])
 {
@@ -59,7 +79,8 @@ int main(int argc, char *argv[])
     if(!philo || !forks)
         return free_structs(set, philo, forks, EXIT_FAILURE);
     create_philo(set, philo, forks);
-    print_id_related_forks(philo, forks);
+    print_id_related_forks(philo, forks); //TORM
+    philosopher(set, philo, forks);
     free_structs(set, philo, forks, EXIT_SUCCESS);
     return (0);
 }
