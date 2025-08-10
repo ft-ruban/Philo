b39f097d93840d9b8d3a0e42b8379013dedcd99b
Lea Devoudelle-Royer <ldevoude@z1r10p5.42lyon.fr>
2025-08-10 17:15:39 +0200

    norme,cleanup(philo.c): now philo.c is properly normed

--- philo/srcs/exec/philo.c
+++ philo/srcs/exec/philo.c
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <ldevoude@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/10 16:08:41 by ldevoude          #+#    #+#             */
+/*   Updated: 2025/08/10 17:07:34 by ldevoude         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <sys/time.h> //gettingtimeofdayneedit
 #include "exec.h"
-#include <unistd.h> //usleep
+#include <sys/time.h> //gettingtimeofdayneedit
+#include <unistd.h>   //usleep
 
 static long	fill_now(void)
 {
@@ -11,60 +22,68 @@ static long	fill_now(void)
 	return (tv.tv_sec * 1000000 + tv.tv_usec);
 }
 
-static int create_thread(t_philo *philo, t_philo *tmp, long now, bool even)
+static int	creating_thread(t_philo *philo, bool even, long now)
 {
-    tmp = philo;
-    while(philo)
-    {
-        now = fill_now();
-        pthread_mutex_lock(&philo->t_alive_mutex);
-        philo->t_alive = now;
-        pthread_mutex_unlock(&philo->t_alive_mutex);
-        if(even)
-        {
-            if(pthread_create(&philo->thread_id, NULL, &routine_even, philo) != 0)
-                return(RETURN_FAILURE);
-            even = false;
-        }
-        else
-        {
-            if (pthread_create(&philo->thread_id, NULL, &routine_odd, philo) != 0)
-                return(RETURN_FAILURE);
-            even = true;
-        }
-        philo = philo->next;
-    }
-    if(pthread_create(&tmp->set->monitor_thread_id, NULL, &philo_monitor, tmp) != 0)
-        return(RETURN_FAILURE);
-    return(RETURN_SUCCESS);
+	while (philo)
+	{
+		now = fill_now();
+		pthread_mutex_lock(&philo->t_alive_mutex);
+		philo->t_alive = now;
+		pthread_mutex_unlock(&philo->t_alive_mutex);
+		if (even)
+		{
+			if (pthread_create(&philo->thread_id, NULL, &routine_even,
+					philo) != 0)
+				return (RETURN_FAILURE);
+			even = false;
+		}
+		else
+		{
+			if (pthread_create(&philo->thread_id, NULL, &routine_odd,
+					philo) != 0)
+				return (RETURN_FAILURE);
+			even = true;
+		}
+		philo = philo->next;
+	}
+	return (RETURN_SUCCESS);
 }
 
-static int join_thread(t_philo *philo)
+static int	create_thread(t_philo *philo, t_philo *tmp, bool even)
 {
-
-    if(pthread_join(philo->set->monitor_thread_id, NULL))
-        return(RETURN_FAILURE); //protect
-    while(philo)
-    {
-        if(pthread_join(philo->thread_id, NULL))
-            return(RETURN_FAILURE); //protect?
-        philo = philo->next;
-    }
-    return(RETURN_SUCCESS);
+	tmp = philo;
+	creating_thread(philo, even, 0);
+	if (pthread_create(&tmp->set->monitor_thread_id, NULL, &philo_monitor,
+			tmp) != 0)
+		return (RETURN_FAILURE);
+	return (RETURN_SUCCESS);
 }
 
-void philosopher(t_settings *set, t_philo *philo, t_forks *forks)
+static int	join_thread(t_philo *philo)
 {
-    struct timeval tv;
+	if (pthread_join(philo->set->monitor_thread_id, NULL))
+		return (RETURN_FAILURE);
+	while (philo)
+	{
+		if (pthread_join(philo->thread_id, NULL))
+			return (RETURN_FAILURE);
+		philo = philo->next;
+	}
+	return (RETURN_SUCCESS);
+}
+// protect create_thread + retour positif si success et aussi protect join?
 
-    gettimeofday(&tv, NULL);
-    set->subunit = tv.tv_sec;
-    set->subusec = tv.tv_usec;
-    gettimeofday(&tv, NULL);
-    set->time_passed = (tv.tv_sec -  set->subunit) * 1000000 + (tv.tv_usec - set->subusec);
-    create_thread(philo, NULL, 0, true); //TOPROTECT
-        //retour d'erreur
-    join_thread(philo);
-    forks = NULL;
-    //retour positif
+void	philosopher(t_settings *set, t_philo *philo, t_forks *forks)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	set->subunit = tv.tv_sec;
+	set->subusec = tv.tv_usec;
+	gettimeofday(&tv, NULL);
+	set->time_passed = (tv.tv_sec - set->subunit) * 1000000 + (tv.tv_usec
+			- set->subusec);
+	create_thread(philo, NULL, true);
+	join_thread(philo);
+	forks = NULL;
 }
