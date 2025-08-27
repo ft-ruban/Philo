bb40d8d05af84f1a07ad924251ba7def6c7f3790
Ruban <Ruban@example.com>
2025-08-27 22:08:08 +0200

    misc

--- philo/srcs/exec/philo_monitor.c
+++ philo/srcs/exec/philo_monitor.c
@@ -25,10 +25,27 @@ static long	fill_now_variable(long *now)
 	return (RETURN_SUCCESS);
 }
 
-static int	philo_monitor_loop_threads(t_philo *tmp, long now)
+static int philo_died_detected(t_philo *tmp)
 {
 	struct timeval	tv;
 
+	pthread_mutex_lock(&tmp->set->print_mutex);
+	if (!tmp->set->death)
+	{
+		gettimeofday(&tv, NULL);
+		tmp->set->time_passed = (tv.tv_sec - tmp->set->subunit)
+			* 1000000 + (tv.tv_usec - tmp->set->subusec);
+		pthread_mutex_lock(&tmp->set->death_mutex);
+		tmp->set->death = true;
+		pthread_mutex_unlock(&tmp->set->death_mutex);
+		printf("%ld %ld died\n", tmp->set->time_passed / 1000, tmp->id);
+	}
+	pthread_mutex_unlock(&tmp->set->print_mutex);
+	return (1);
+}
+
+static int	philo_monitor_loop_threads(t_philo *tmp, long now)
+{
 	while (tmp)
 	{
 		pthread_mutex_lock(&tmp->t_alive_mutex);
@@ -37,19 +54,7 @@ static int	philo_monitor_loop_threads(t_philo *tmp, long now)
 			&& tmp->meals_eaten != tmp->set->max_meal)
 		{
 			pthread_mutex_unlock(&tmp->t_alive_mutex);
-			pthread_mutex_lock(&tmp->set->print_mutex);
-			if (!tmp->set->death)
-			{
-				gettimeofday(&tv, NULL);
-				tmp->set->time_passed = (tv.tv_sec - tmp->set->subunit)
-					* 1000000 + (tv.tv_usec - tmp->set->subusec);
-				pthread_mutex_lock(&tmp->set->death_mutex);
-				tmp->set->death = true;
-				pthread_mutex_unlock(&tmp->set->death_mutex);
-				printf("%ld %ld died\n", tmp->set->time_passed / 1000, tmp->id);
-			}
-			pthread_mutex_unlock(&tmp->set->print_mutex);
-			return (1);
+			return(philo_died_detected(tmp));
 		}
 		pthread_mutex_unlock(&tmp->t_alive_mutex);
 		tmp = tmp->next;
@@ -64,26 +69,20 @@ static int	philo_monitor_loop_threads(t_philo *tmp, long now)
 
 void	*philo_monitor(void *arg)
 {
-	t_philo			*tmp;
 	t_philo			*start;
 	long			now;
 
 	now = 0;
 	start = arg;
-	tmp = arg;
-	usleep(tmp->set->t_die);
+	usleep(start->set->t_die);
 	pthread_mutex_lock(&start->set->pasta_mutex);
 	while (!start->set->death
 		&& start->set->philo_full_pasta != start->set->nbr_philo)
 	{
 		pthread_mutex_unlock(&start->set->pasta_mutex);
 		usleep(500);
-		tmp = start;
-		if (philo_monitor_loop_threads(tmp, now))
+		if (philo_monitor_loop_threads(start, now))
 			return (NULL);
-		//usleep(tmp->set->t_die / 2);
-		//usleep(tmp->set->t_die);
-		
 		pthread_mutex_lock(&start->set->pasta_mutex);
 	}
 	pthread_mutex_unlock(&start->set->pasta_mutex);
